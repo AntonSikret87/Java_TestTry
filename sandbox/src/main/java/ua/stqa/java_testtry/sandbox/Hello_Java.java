0cebb41331bb9ceaa3e40e4d58763579a6116323
AntonSikret87 <antonkarabeinikov@gmail.com>
2016-02-25 00:02:19 +0200

    Reupload

--- sandbox/src/main/java/ua/stqa/java_testtry/sandbox/Hello_Java.java
+++ sandbox/src/main/java/ua/stqa/java_testtry/sandbox/Hello_Java.java
@@ -1,9 +1,9 @@
 package ua.stqa.java_testtry.sandbox;
 
-public class Hello_Java{
-	
-	public static void main(String[] args){
+public class Hello_Java {
 
-System.out.println("Hello World Java Rock ");
-}
+    public static void main(String[] args) {
+
+        System.out.println("Hello World Java Rock ");
+    }
 }
