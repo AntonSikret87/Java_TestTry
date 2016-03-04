90dc13b8b0aa9d316f7f494c88456192ecb68b1a
AntonSikret87 <antonkarabeinikov@gmail.com>
2016-03-04 21:53:44 +0200

    Create Equetion tests

---
+++ sandbox/src/test/java/ua/stqa/java_testtry/sandbox/EquetionTests.java
@@ -0,0 +1,30 @@
+package ua.stqa.java_testtry.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by sikretSSD on 04.03.2016.
+ */
+public class EquetionTests {
+
+    @Test
+    public void test0(){
+        Equetion e = new Equetion(1,1,1);
+        Assert.assertEquals(e.rootNumber(),0);
+
+    }
+    @Test
+    public void test1(){
+        Equetion e = new Equetion(1,2,1);
+        Assert.assertEquals(e.rootNumber(),1);
+
+    }
+    @Test
+    public void test2(){
+        Equetion e = new Equetion(1,5,6);
+        Assert.assertEquals(e.rootNumber(),2);
+
+    }
+}
+
