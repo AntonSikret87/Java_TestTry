ff646b816bfb5a0b1169813969c1f36731b44cc0
AntonSikret87 <antonkarabeinikov@gmail.com>
2016-04-06 23:37:44 +0300

    #15 Parameterize tests for Creation Contacts and Groups

--- addressbook-web-tests/src/test/java/ua/stqu/pft/addressbook/tests/ContactCreateTests.java
+++ addressbook-web-tests/src/test/java/ua/stqu/pft/addressbook/tests/ContactCreateTests.java
@@ -1,15 +1,52 @@
 package ua.stqu.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ua.stqu.pft.addressbook.model.ContactData;
 import ua.stqu.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreateTests extends TestBase {
+
+
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.xml"))) {
+            String xml = "";
+            String line = reader.readLine();
+            while (line != null) {
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xstream = new XStream();
+            xstream.processAnnotations(ContactData.class);
+            List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+            return contacts.stream().map(g -> new Object[]{g}).collect(Collectors.toList()).iterator();
+        }
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
+        app.goTo().homePage();
+        Contacts before = app.contact().all();
+        app.contact().create(contact);
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
+        assertThat(after, equalTo(before
+                .withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
 
     @Test
     public void newContactCreating() {
