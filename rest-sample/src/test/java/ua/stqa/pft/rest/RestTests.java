0f730dc571708cd665a4058bb86e06c842bb8086
AntonSikret87 <antonkarabeinikov@gmail.com>
2016-04-22 16:38:34 +0300

    rest test start creation

---
+++ rest-sample/src/test/java/ua/stqa/pft/rest/RestTests.java
@@ -0,0 +1,14 @@
+package ua.stqa.pft.rest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by sikretSSD on 21.04.2016.
+ */
+public class RestTests {
+
+    @Test
+    public void testCreateIssue(){
+        
+    }
+}
