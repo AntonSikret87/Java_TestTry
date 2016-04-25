242f054a4d4540c417a1c8fb5c164023587a5b0e
AntonSikret87 <antonkarabeinikov@gmail.com>
2016-04-25 20:09:23 +0300

    setup uncomment

--- addressbook-web-tests/src/test/java/ua/stqu/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqu/pft/addressbook/tests/TestBase.java
@@ -3,7 +3,6 @@ package ua.stqu.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -30,17 +29,17 @@ public class TestBase {
 
     protected static final ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+          //new ApplicationManager(BrowserType.CHROME)
 
-    //  new ApplicationManager(BrowserType.CHROME)
-//    @BeforeSuite
-//    public void setUp() throws Exception {
-//        app.init();
-//    }
     @BeforeSuite
-    public void setUp(ITestContext context) throws Exception {
+    public void setUp() throws Exception {
         app.init();
-        context.setAttribute("app", app);
     }
+//    @BeforeSuite
+//    public void setUp(ITestContext context) throws Exception {
+//        app.init();
+//        context.setAttribute("app", app);
+//    }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() {
